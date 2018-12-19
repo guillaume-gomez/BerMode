b569902dc61b0a00e9d528ae35908d5e38bd8e4a
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-12-20 00:42:54 +0100

    add delay parms

--- app.rb
+++ app.rb
@@ -16,14 +16,20 @@ class BerMode < Sinatra::Base
 
   post '/slack/command' do
     username = params["user_name"]
-    contents, channel_id = params["text"].split("|")
+    
+    function_params = params["text"].split("|")
+    contents = function_params[0]
+    channel_id = function_params[1]
+    delay = function_params[2] || 0.5
+
     contents = contents.split(" ")
-    channel = channel_id ? URI.encode(channel_id.strip) : params["channel_id"]
+    channel = channel_id && channel_id != "" ? URI.encode(channel_id.strip) : params["channel_id"]
+
     threads = []
     token = ENV["SLACK_API_TOKEN"]
     contents.each_with_index do |content, index|
       return if content.empty?
-      threads << Thread.new { sleep(index * 0.4); result = HTTParty.get("https://slack.com/api/chat.postMessage?token=#{token}&channel=#{channel}&text=#{content}&username=#{username}&as_user=false&link_names=true"); puts result }
+      threads << Thread.new { sleep(index * delay.to_i); result = HTTParty.get("https://slack.com/api/chat.postMessage?token=#{token}&channel=#{channel}&text=#{content}&username=#{username}&as_user=false&link_names=true"); puts result }
     end
     #threads.each(&:join)
     puts "end of function"
